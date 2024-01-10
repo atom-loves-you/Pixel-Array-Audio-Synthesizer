15521e1080226b233ca12437c8af06114dbb6449
Khristin <atom.loves.you.2022@gmail.com>
2024-01-10 08:43:08 -0800

    Update

--- Pixel-Array-Audio-Synthesizer-Video-Music-Controller/src/script.js
+++
@@ -1,6 +0,0 @@
-let spans = document.querySelectorAll('span');
-Array.from(spans).forEach(span => {
-  span.addEventListener('click', () => {
-    document.body.style.backgroundColor = span.style.backgroundColor;    
-  });
-});
